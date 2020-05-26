5882896a5fb75406e9cef113aa43997befd1a823
Sri <srinadha.kolli@gmail.com>
2020-05-26 08:15:13 -0400

    Constructor Practice

---
+++ JavaPractice/src/com/sripractice/array/string/Constructor.java
@@ -0,0 +1,11 @@
+package com.sripractice.array.string;
+
+public class Constructor {
+
+	Constructor(){
+		System.out.println("This is from constructor");
+	}
+	public static void main(String[] args) {
+		Constructor obj = new Constructor();
+	}
+}
